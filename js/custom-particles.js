9b6043c673439a2333c4086f614841b0aa83e760
Baptiste Vadé <sundayplayer@outlook.fr>
2017-11-17 14:38:21 +0100

    Minor modification of particles + cleaning

--- js/custom-particles.js
+++ js/custom-particles.js
@@ -1,7 +1,7 @@
 particlesJS("particles-js", {
     "particles": {
         "number": {
-            "value": 190,
+            "value": 150,
             "density": {
                 "enable": true,
                 "value_area": 800
@@ -43,14 +43,14 @@ particlesJS("particles-js", {
         },
         "move": {
             "enable": true,
-            "speed": 3,
+            "speed": 1,
             "direction": "none",
             "random": false,
             "straight": false,
             "out_mode": "bounce",
             "bounce": false,
             "attract": {
-                "enable": true,
+                "enable": false,
                 "rotateX": 600,
                 "rotateY": 1200
             }
@@ -70,27 +70,7 @@ particlesJS("particles-js", {
             "resize": true
         },
         "modes": {
-            "grab": {
-                "distance": 140,
-                "line_linked": {
-                    "opacity": 1
-                }
-            },
-            "bubble": {
-                "distance": 400,
-                "size": 40,
-                "duration": 2,
-                "opacity": 8,
-                "speed": 3
-            },
-            "repulse": {
-                "distance": 200,
-                "duration": 0.4
-            },
             "push": {
-                "particles_nb": 5
-            },
-            "remove": {
                 "particles_nb": 2
             }
         }
